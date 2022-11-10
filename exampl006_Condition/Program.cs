a9f5a8ecf4147bfedfbd4e76610c3aa8607fd9b3
Natali <vipnatalii@bk.ru>
2022-11-10 17:51:46 +0500

    exampl006_Conditin

---
+++ exampl006_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ползователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Ангелина")
+{
+    Console.WriteLine("Ура, это же Ангелина ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
